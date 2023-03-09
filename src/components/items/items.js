d436c651041bc0e031207891584ae2f1609065ff
Devansh Gupta <63590510+GraphicsMonster@users.noreply.github.com>
2023-03-09 17:23:01 +0530

    temporary fix for the hierarchy issue which would't allow npm to locate the index.html file

---
+++ src/components/items/items.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import "./items.css"
+
+function Items() {
+  return (
+    <div className='items'>
+        <div className='items-container'>
+            <div className='items-li'>
+            <div className='items-li-container'>
+                    <p>Housing</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+            <div className='items-li-container'>
+                    <p>Education</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+            <div className='items-li-container'>
+                    <p>Food</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+            <div className='items-li-container'>
+                    <p>Clothes</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+                <div className='items-li-container'>
+                    <p>Entertainment</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+                <div className='items-li-container'>
+                    <p>Healthcare</p>
+                    <hr />
+                </div>
+            </div>
+             <div className='items-li'>
+                <div className='items-li-container'>
+                    <p>Insurance</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+                <div className='items-li-container'>
+                    <p>Transportation</p>
+                    <hr />
+                </div>
+            </div>
+            <div className='items-li'>
+                <div className='items-li-container'>
+                    <p>Reading</p>
+                    <hr />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Items
